04eb6f6ab3b68c3520d8b0d9c4ceafe0485fa55b
allansp9 <allansp9@gmail.com>
2019-12-09 20:44:08 -0300

    Updates

---
+++ precache-manifest.dd5895aba2741b888e4c54684c6b3c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ecc2315381ab43794763f33469e578",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "91fe18a2ca84a0505171",
+    "url": "/monsters-rolodex/static/css/main.0cfc1859.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "91fe18a2ca84a0505171",
+    "url": "/monsters-rolodex/static/js/main.b5f6cc7f.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
